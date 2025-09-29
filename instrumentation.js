7aa75ba4396ff0acddcd15e93193bdacc860002e
Danvick <danvickmiller@live.com>
2025-09-29 15:52:00 +0300

    feat: also set secrets in edge environment

--- instrumentation.js
+++ instrumentation.js
@@ -19,4 +19,22 @@ export async function register() {
             console.error('Failed to fetch secret from Parameter Store:', error);
         }
     }
+
+    if (process.env.NEXT_RUNTIME === 'edge') {
+        try {
+            const ssm = new SSMClient({ region: 'eu-west-2' });
+            const command = new GetParameterCommand({
+                Name: `/amplify/shared/${process.env.APP_ID}/CLERK_SECRET_KEY`,
+                WithDecryption: true, // Needed if the parameter is a SecureString
+            });
+
+            const response = await ssm.send(command);
+            if (response.Parameter?.Value) {
+                process.env.CLERK_SECRET_KEY = response.Parameter.Value;
+                console.log('Successfully fetched CLERK_SECRET_KEY from Parameter Store');
+            }
+        } catch (error) {
+            console.error('Failed to fetch secret from Parameter Store:', error);
+        }
+    }
 }
